2b299aff18ff254055db3ef1cd491c1de95caea0
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2026-02-23 16:11:11 -0500

    Add greenhouse build_url tests

--- tests/test_sites.py
+++ tests/test_sites.py
@@ -1,18 +1,33 @@
 import pytest
-from jobsearch.sites.linkedin import build_url
+from jobsearch.sites import linkedin, greenhouse
 
 @pytest.mark.parametrize("term", ["python", "django", "fastapi", "backend", "pytest"])
 def test_linkedin_url_single(term):
-    url = build_url(term)
+    url = linkedin.build_url(term)
     assert url == f"https://www.linkedin.com/jobs/search/?keywords={term}&location=Boston%2C+MA&f_TPR=r86400"
 
 def test_linkedin_url_location():
     term = "python"
-    url = build_url(term, "New York, NY")
+    url = linkedin.build_url(term, "New York, NY")
     assert url == f"https://www.linkedin.com/jobs/search/?keywords={term}&location=New+York%2C+NY&f_TPR=r86400"
 
-def test_linkedin_url_multiple():
+def test_linkedin_url_compound():
     term = "backend engineer"
-    url = build_url(term)
+    url = linkedin.build_url(term)
     assert url == f"https://www.linkedin.com/jobs/search/?keywords=backend+engineer&location=Boston%2C+MA&f_TPR=r86400"
     
+@pytest.mark.parametrize("term", ["python", "django", "fastapi", "backend", "pytest"])
+def test_greenhouse_url_single(term):
+    url = greenhouse.build_url(term)
+    assert url == f"https://my.greenhouse.io/jobs?query={term}&lat=42.355492&lon=-71.048611"
+
+def test_greenhouse_url_location():
+    term = "python"
+    lat, lon = "40.68295", "-73.9708"
+    url = greenhouse.build_url(term, lat, lon)
+    assert url == f"https://my.greenhouse.io/jobs?query=python&lat={lat}&lon={lon}"
+
+def test_greenhouse_url_compound():
+    term = "backend engineer"
+    url = greenhouse.build_url(term)
+    assert url == f"https://my.greenhouse.io/jobs?query=backend%20engineer&lat=42.355492&lon=-71.048611"
