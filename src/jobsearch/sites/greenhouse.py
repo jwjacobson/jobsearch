a0ab2419ae46be8e84df75b4aef146a4434a616d
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2026-02-23 16:11:26 -0500

    Add Greenhouse build_url module

---
+++ src/jobsearch/sites/greenhouse.py
@@ -0,0 +1,9 @@
+from urllib import parse
+
+BASE_URL = "https://my.greenhouse.io/jobs"
+BOSTON_LAT = "42.355492"
+BOSTON_LON = "-71.048611"
+
+def build_url(term: str, lat: str = BOSTON_LAT, lon: str = BOSTON_LON) -> str:
+    term = parse.quote(term)
+    return f"{BASE_URL}?query={term}&lat={lat}&lon={lon}"
