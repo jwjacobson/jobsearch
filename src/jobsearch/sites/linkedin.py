9906480d3af6289e345daafb705acb378f2fe1b0
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2026-02-23 15:50:58 -0500

    Add LinkedIn site config

---
+++ src/jobsearch/sites/linkedin.py
@@ -0,0 +1,9 @@
+from urllib import parse
+
+BASE_URL = "https://www.linkedin.com/jobs/search/"
+TIMESPAN = "f_TPR=r86400"
+
+def build_url(term: str, location: str = "Boston, MA") -> str:
+    term = parse.quote_plus(term)
+    location = parse.quote_plus(location)
+    return f"{BASE_URL}?keywords={term}&location={location}&{TIMESPAN}"
