42bcbad6564236d8fdb6fad58fcd0da0faae3ff2
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2026-02-24 14:40:05 -0500

    Add browser.py browser launcher

---
+++ src/jobsearch/browser.py
@@ -0,0 +1,32 @@
+from decouple import config
+from playwright.sync_api import sync_playwright
+
+from .sites import linkedin, greenhouse, builtin
+
+SITES = {
+    "linkedin": linkedin.build_url,
+    "greenhouse": greenhouse.build_url,
+    "builtin": builtin.build_url,
+}
+USER_DATA_DIR = config("USER_DATA_DIR", None)
+
+def run(settings):
+    with sync_playwright() as p:
+        if USER_DATA_DIR:
+            print(f"Accessing user data at {USER_DATA_DIR}")
+            context = p.chromium.launch_persistent_context(
+                user_data_dir=USER_DATA_DIR,
+                headless=False)
+        else:
+            print("No user data found; please set USER_DATA_DIR in .env")
+            browser = p.chromium.launch(headless=False)
+            context = browser.new_context()
+
+        for site in settings["sites"]["enabled"]:
+            for term in settings["search"]["terms"]:
+                page = context.new_page()
+                url = SITES[site](term)
+                page.goto(url)
+
+        input("Press Enter to close browser...")
+        context.close()
