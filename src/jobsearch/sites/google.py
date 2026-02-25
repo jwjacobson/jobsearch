5ebbb9f2de84b9326a3bfb9967cc6df86e7b4918
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2026-02-25 10:00:01 -0500

    Add google module

---
+++ src/jobsearch/sites/google.py
@@ -0,0 +1,28 @@
+"""
+This module replicates the google searches performed by Brian's Job Search - https://briansjobsearch.com/ 
+"""
+
+from urllib import parse
+
+BASE_URL = "https://www.google.com/search?q="
+SITES = [
+    "lever.co",
+    "ashbyhq.com",
+    "jobs.*",
+    "careers.*+OR+site%3A*%2Fcareers%2F*+OR+site%3A*%2Fcareer%2F*",
+    "people.*",
+    "talent.*",
+    "workatastartup.com"
+    "icims.com",
+    "wellfound.com",
+    "breezy.hr"
+]
+LOCATION = "massachusetts"
+TIMESPAN = "tbs=qdr:d"
+
+def build_url(term: str, site: str, location: str = LOCATION) -> str:
+    term = parse.quote(term)
+    return f'{BASE_URL}"{term}"%20site%3A{site}%20{location}&{TIMESPAN}'
+
+def get_urls(term: str) -> list[str]:
+    return [build_url(term, site) for site in SITES]
