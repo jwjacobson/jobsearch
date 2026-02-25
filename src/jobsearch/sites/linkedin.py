70c8ac171789879e6d3ba351aa1ff5df75d7d4ee
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2026-02-25 10:37:06 -0500

    Put linkedin location in top-level constant

--- src/jobsearch/sites/linkedin.py
+++ src/jobsearch/sites/linkedin.py
@@ -1,9 +1,10 @@
 from urllib import parse
 
 BASE_URL = "https://www.linkedin.com/jobs/search/"
+LOCATION = "Boston, MA"
 TIMESPAN = "f_TPR=r86400" # last day
 
-def build_url(term: str, location: str = "Boston, MA") -> str:
+def build_url(term: str, location: str = LOCATION) -> str:
     term = parse.quote_plus(term)
     location = parse.quote_plus(location)
     return f"{BASE_URL}?keywords={term}&location={location}&{TIMESPAN}"
