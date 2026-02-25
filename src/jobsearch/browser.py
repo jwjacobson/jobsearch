7a580a51ee514a7e7bc730ede51bda35849cb73b
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2026-02-25 10:55:48 -0500

    Remove debug statements from browser.py

--- src/jobsearch/browser.py
+++ src/jobsearch/browser.py
@@ -20,12 +20,10 @@ USER_DATA_DIR = config("USER_DATA_DIR", None)
 def run(settings):
     with sync_playwright() as p:
         if USER_DATA_DIR:
-            print(f"Accessing user data at {USER_DATA_DIR}")
             context = p.chromium.launch_persistent_context(
                 user_data_dir=USER_DATA_DIR,
                 headless=False)
         else:
-            print("No user data found; please set USER_DATA_DIR in .env")
             browser = p.chromium.launch(headless=False)
             context = browser.new_context()
 
